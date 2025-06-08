303d624a9cbce85682dce066e31c25c6c1226b8d
ChristopherSemyonov <mellybang.lav@gmail.com>
2025-06-09 00:09:05 +0300

    Updated layout, add alternating rows and much more

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.getElementById('toggle-theme').addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode')
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('theme-toggle')
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const savedTheme = localStorage.getItem('theme')
+  let theme = savedTheme || (prefersDark ? 'dark' : 'light')
+
+  document.documentElement.setAttribute('data-theme', theme)
+  btn.textContent = theme === 'dark' ? '🌙' : '☀️'
+
+  btn.addEventListener('click', () => {
+    theme =
+      document.documentElement.getAttribute('data-theme') === 'dark'
+        ? 'light'
+        : 'dark'
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+    btn.textContent = theme === 'dark' ? '🌙' : '☀️'
+  })
 })
